b5bae45b1b8d96f987e4b7e46dce89e64d4cbbfb
bao-dimage <bao-nguyen@dimage.co.jp>
2023-02-21 16:36:10 +0700

    Add Message when Input Error

--- timesheet-app/resources/views/timesheet/edit.blade.php
+++ timesheet-app/resources/views/timesheet/edit.blade.php
@@ -15,16 +15,20 @@
                         <div class="col-md-12 mb-3">
                             <label for="">Title</label>
                             <input type="text" name="title" value="{{$timesheet->title}}" class="form-control">
-                            <!-- @error('title') <small class="text-danger">{{$message}}</small> @enderror -->
+                            @error('title') <small class="text-danger">{{$message}}</small> @enderror
                         </div>
                         <div class="col-md-6 mb-3">
                             <label for="">Diff Work</label>
                             <input type="text" name="diff_work" value="{{$timesheet->diff_work}}" class="form-control">
+                            @error('diff_work') <small class="text-danger">{{$message}}</small> @enderror
+
                         </div>
 
                         <div class="col-md-6 mb-3">
                             <label for="">Plan Work</label>
                             <input type="text" name="plan_work" value="{{$timesheet->plan_work}}" class="form-control">
+                            @error('plan_work') <small class="text-danger">{{$message}}</small> @enderror
+
                         </div>
                         <div class="col-md-12 mb-3">
                             <button type="submit" class="btn btn-primary float-end">Submit</button>
