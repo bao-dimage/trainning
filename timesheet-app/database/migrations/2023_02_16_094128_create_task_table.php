e0275957ad7735b2e77761dbf0eee4f4bccce25f
bao-dimage <bao-nguyen@dimage.co.jp>
2023-02-21 16:33:58 +0700

    Default value to created_at Default value to updated_at

--- timesheet-app/database/migrations/2023_02_16_094128_create_task_table.php
+++ timesheet-app/database/migrations/2023_02_16_094128_create_task_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
             
             $table->unsignedBigInteger('timesheet_id');
             $table->foreign('timesheet_id')->references('timesheet_id')->on('timesheets');
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
             $table->timestamps();
         });
     }
