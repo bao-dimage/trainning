639607cb0fb7f4d38c056cec0d9b35ea8f3e1e74
bao-dimage <bao-nguyen@dimage.co.jp>
2023-03-17 14:14:54 +0700

    Delete User With Modal Confirm

--- timesheet-app/app/Http/Livewire/ManageUser/Index.php
+++ timesheet-app/app/Http/Livewire/ManageUser/Index.php
@@ -7,6 +7,31 @@ use Livewire\Component;
 
 class Index extends Component
 {
+    public $user_id;
+
+    public function deleteUser($user_id){
+      
+        $this->user_id = $user_id;
+     
+    }
+
+    
+
+    public function destroyUser()
+    {
+        // delete
+        // dd($this->user);
+        $user = User::find($this->user_id);
+
+        // dd($User);
+        $user->delete();
+        $this->dispatchBrowserEvent('hide-delete-modal');
+   
+        
+
+        session() ->flash('message','User Deleted');
+       
+    }
     public function render()
     {
         $users = User::all();
