405990ad4d4e72e7a18331582724c86bc7d70dc6
bao-dimage <bao-nguyen@dimage.co.jp>
2023-03-21 15:56:49 +0700

    Create TimesheetRepositoryInterface

---
+++ timesheet-app/app/Repositories/Interfaces/TimesheetRepositoryInterface.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Repositories\Interfaces;
+
+Interface TimesheetRepositoryInterface{
+    
+    
+    public function storeTimesheet($data);
+
+    public function findTasks($timesheet);
+    public function findTimesheet($id);
+    public function updateTimesheet($data, $timesheet); 
+    public function destroyTimesheet($id);
+}
